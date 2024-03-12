07a07d623cd801f1ce2f52248631c09924bceb07
Aamir Raza <merciful9795@gmail.com>
2024-03-12 23:42:30 +0530

    in this commit frontend and backend combined in one folder and users, cart and order backend code implemented.

---
+++ mern-stack-e-commerce-project/backend/index.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const server = express();
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const { createProduct } = require("./controller/Product");
+const productsRouter = require("./routes/Products");
+const categoriesRouter = require("./routes/Categories");
+const brandsRouter = require("./routes/Brands");
+const usersRouter = require("./routes/Users");
+const authRouter = require("./routes/Auth");
+const cartRouter = require("./routes/Cart");
+const ordersRouter = require("./routes/Order");
+
+//middlewares
+
+server.use(
+  cors({
+    exposedHeaders: ["X-Total-Count"],
+  })
+);
+server.use(express.json()); // to parse req.body
+server.use("/products", productsRouter.router);
+server.use("/categories", categoriesRouter.router);
+server.use("/brands", brandsRouter.router);
+server.use("/users", usersRouter.router);
+server.use("/auth", authRouter.router);
+server.use("/cart", cartRouter.router);
+server.use("/orders", ordersRouter.router);
+
+main().catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect("mongodb://127.0.0.1:27017/ecommerce");
+  console.log("database connected");
+}
+
+server.get("/", (req, res) => {
+  res.json({ status: "success" });
+});
+
+server.listen(8080, () => {
+  console.log("server started");
+});
