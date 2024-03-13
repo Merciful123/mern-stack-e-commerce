87d4f28a908bee4e7958d806ce1696fd360034bd
Aamir Raza <merciful9795@gmail.com>
2024-03-13 13:03:31 +0530

    passport js implemented with local jwt

--- backend/model/Users.js
+++ backend/model/Users.js
@@ -1,19 +1,15 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
-const userSchema = new Schema(
-  {
-    email: { type: String, required: true, unique: true },
-    password: { type: Buffer, required: true },
-    role: { type: String, required: true, default: "user" },
-    addresses: { type: [Schema.Types.Mixed] },
-    // for addresses, we can make a separate Schema like orders. but in this case we are fine
-    name: { type: String },
-    salt: Buffer,
-    resetPasswordToken: { type: String, default: "" },
-  },
-  { timestamps: true }
-);
+const userSchema = new Schema({
+  email: { type: String, required: true, unique: true },
+  password: { type: Buffer, required: true },
+  role: { type: String, required: true, default: "user" },
+  addresses: { type: [Schema.Types.Mixed] },
+  // TODO:  We can make a separate Schema for this
+  name: { type: String },
+  salt: Buffer,
+});
 
 const virtual = userSchema.virtual("id");
 virtual.get(function () {
