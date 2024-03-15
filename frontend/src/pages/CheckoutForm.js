ec0ab2e7e6e528b63c1cdf87689f37bff80fe7c0
Aamir Raza <merciful9795@gmail.com>
2024-03-15 19:45:52 +0530

    minor fix

--- frontend/src/pages/CheckoutForm.js
+++ frontend/src/pages/CheckoutForm.js
@@ -61,7 +61,7 @@ export default function CheckoutForm() {
       elements,
       confirmParams: {
         // Make sure to change this to your payment completion page
-        return_url: `https://mern-ecommerce-lyart.vercel.app/order-success/${currentOrder.id}`,
+        return_url: `http://localhost:8080/${currentOrder.id}`,
       },
     });
 
