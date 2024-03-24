bc096fc09a9a4c9544a56d4076ac92822f552acc
Aamir Raza <merciful9795@gmail.com>
2024-03-25 02:19:12 +0530

    minor fix, deploy

--- frontend/src/pages/OrderSuccessPage.js
+++ frontend/src/pages/OrderSuccessPage.js
@@ -18,7 +18,7 @@ function OrderSuccessPage() {
 
   return (
     <>
-      {!params.id && <Navigate to="/" replace={true}></Navigate>}
+      {!params?.id && <Navigate to="/" replace={true}></Navigate>}
       <main className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
         <div className="text-center">
           <p className="text-base font-semibold text-indigo-600">
@@ -28,7 +28,7 @@ function OrderSuccessPage() {
             Order Number #{params?.id}
           </h1>
           <p className="mt-6 text-base leading-7 text-gray-600">
-            You can check your order in My Account > My Orders
+            You can check your order in My Account {">"} My Orders
           </p>
           <div className="mt-10 flex items-center justify-center gap-x-6">
             <Link
